0037fdee4aef9c21010d4ab88453ce4c1f4e136a
wael2000 <weldoami@redhat.com>
2022-03-16 20:52:04 +0400

    first commit

---
+++ src/main/java/org/redhat/Serverless.java
@@ -0,0 +1,25 @@
+package org.redhat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.QueryParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.DefaultValue;
+
+@Path("/salary")
+public class Serverless {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String,Double> net(@QueryParam("growth") @DefaultValue("0") Integer growth) {
+        Map<String,Double> map= new HashMap<>();
+        map.put("salary", growth*0.8);
+        return map;
+    }
+
+}
